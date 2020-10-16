1c08949561632bb399f605638546d0384d83f897
Pinki Kumari <Pinki Kumari@DESKTOP-2CII4BM>
2020-10-16 09:57:25 +0530

    Account added through branch1

---
+++ OnlineShopping/src/com/lti/model/Account.java
@@ -0,0 +1,24 @@
+package com.lti.model;
+
+public class Account {
+
+	int AccountId;
+	String accountHolderName;
+
+	public int getAccountId() {
+		return AccountId;
+	}
+
+	public void setAccountId(int accountId) {
+		AccountId = accountId;
+	}
+
+	public String getAccountHolderName() {
+		return accountHolderName;
+	}
+
+	public void setAccountHolderName(String accountHolderName) {
+		this.accountHolderName = accountHolderName;
+	}
+
+}
