bbd169f3988de65dffb12c18e839adb3e2206482
Pinki Kumari <Pinki Kumari@DESKTOP-2CII4BM>
2020-10-16 09:47:18 +0530

    Transaction added

--- OnlineShopping/src/com/lti/model/Transaction.java
+++ OnlineShopping/src/com/lti/model/Transaction.java
@@ -1,5 +1,26 @@
 package com.lti.model;
 
+import java.time.LocalDate;
+
 public class Transaction {
+
+	int transactionId;
+	LocalDate transactionDate;
+
+	public int getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(int transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	public LocalDate getTransactionDate() {
+		return transactionDate;
+	}
+
+	public void setTransactionDate(LocalDate transactionDate) {
+		this.transactionDate = transactionDate;
+	}
 
 }
